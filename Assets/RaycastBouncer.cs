8305e5efbb6ef184afe67b6583e6a02a0ae46b77
Michele Rossi <mikymod@gmail.com>
2021-12-03 21:56:29 +0100

    fix raycastBouncer

--- Assets/RaycastBouncer.cs
+++ Assets/RaycastBouncer.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class RaycastBouncer : MonoBehaviour
 {
     private LineRenderer lineRenderer;
-    private List<Vector3> positions;
+    private Vector3[] positions;
     private int numRaycast = 10;
     private Vector3 inDirection;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        positions = new List<Vector3>();
+        positions = new Vector3[10];
     }
 
     private void Update()
     {
         Vector3 rayOrigin = transform.position;
         inDirection = Camera.main.transform.forward;
+        
         lineRenderer.positionCount = 1;
-        positions.Add(rayOrigin);
+        positions[0] = rayOrigin;
 
-        for (int i = 0; i < numRaycast; i++)
+        for (int i = 1; i < numRaycast; i++)
         {
-            positions[i] = rayOrigin;
             if (Physics.Raycast(rayOrigin, inDirection, out RaycastHit hit))
             {
                 rayOrigin = hit.point;
                 inDirection = Vector3.Reflect(inDirection, hit.normal);
                 lineRenderer.positionCount++;
-                positions.Add(rayOrigin);
+                positions[i] = rayOrigin;
 
                 Debug.DrawLine(hit.point, hit.point + hit.normal, Color.green);
                 Debug.DrawLine(hit.point, hit.point + inDirection, Color.blue);
@@ -41,6 +41,6 @@ public class RaycastBouncer : MonoBehaviour
             }
         }
 
-        lineRenderer.SetPositions(positions.ToArray());
+        lineRenderer.SetPositions(positions);
     }
 }
