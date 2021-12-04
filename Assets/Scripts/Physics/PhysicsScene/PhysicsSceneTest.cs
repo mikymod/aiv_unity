079b987c869e9d20a67b42b346ada48a3e979625
Michele Rossi <mikymod@gmail.com>
2021-12-04 10:07:43 +0100

    add homework 28

--- Assets/Scripts/Physics/PhysicsScene/PhysicsSceneTest.cs
+++ Assets/Scripts/Physics/PhysicsScene/PhysicsSceneTest.cs
@@ -20,6 +20,11 @@ public class PhysicsSceneTest : MonoBehaviour
     GameObject sim_ball;
     private Rigidbody realBallRB;
     private Material realBallMat;
+    private LineRenderer realBallLineRenderer;
+
+    // [SerializeField] private Rigidbody[] spheres;
+    [SerializeField] private LineRenderer[] renderers;
+    private Rigidbody[] sim_spheres;
 
     void Start()
     {
@@ -40,6 +45,8 @@ public class PhysicsSceneTest : MonoBehaviour
 
         realBallRB = real_ball.GetComponent<Rigidbody>();
         realBallMat = real_ball.GetComponent<Renderer>().material;
+
+        realBallLineRenderer = real_ball.GetComponent<LineRenderer>();
     }
 
     void ClearTrajectory()
@@ -57,6 +64,14 @@ public class PhysicsSceneTest : MonoBehaviour
         {
             return;
         }
+        // else if (realBallRB.IsSleeping() && !Input.GetMouseButton(0))
+        // {
+        //     realBallLineRenderer.positionCount = 0;
+        //     for (int i = 0; i < renderers.Length; i++)
+        //     {
+        //         renderers[i].positionCount = 0;
+        //     }
+        // }
 
         if (Input.GetMouseButton(0))
         {
@@ -71,9 +86,19 @@ public class PhysicsSceneTest : MonoBehaviour
                 lastHitPoint = hit.point;
                 currForce = hit.point - real_ball.transform.position;
                 currForce = Vector3.Scale(currForce, new Vector3(ForceFactor, 0, ForceFactor));
-                SimulatePath();
+                SimulatePath(); 
             }
         }
+        
+        // if (realBallRB.IsSleeping() && !Input.GetMouseButton(0)) 
+        // {
+        //     realBallLineRenderer.positionCount = 0;
+        //     for (int i = 0; i < renderers.Length; i++)
+        //     {
+        //         renderers[i].positionCount = 0;
+        //     }
+        // }
+
         if (Input.GetMouseButtonUp(0))
         {
             real_ball.GetComponent<Rigidbody>().AddForce(currForce, ForceMode.Impulse);
@@ -82,26 +107,51 @@ public class PhysicsSceneTest : MonoBehaviour
 
     private void SimulatePath()
     {
-        ClearTrajectory();     //removes all previously simulated balls
+        realBallLineRenderer.positionCount = 0;
         
         //set the simulation white ball back to the original position
         sim_ball = Instantiate(BallPrefab, real_ball.transform.position, real_ball.transform.rotation);
         SceneManager.MoveGameObjectToScene(sim_ball, physicsScene);
         Rigidbody sim_rb = sim_ball.GetComponent<Rigidbody>();
 
+        // Instantiate balls
+        List<GameObject> simOthers = new List<GameObject>();
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            var sim_sphere = Instantiate(BallPrefab, renderers[i].transform.position, renderers[i].transform.rotation);
+            SceneManager.MoveGameObjectToScene(sim_sphere, physicsScene); 
+            simOthers.Add(sim_sphere);
+        }
+
         //simulate a specific number of frames into the future
         sim_rb.AddForce(currForce, ForceMode.Impulse);
+
+        // Set line renderers position counts
+        realBallLineRenderer.positionCount = SimSteps;
+        for (int j = 0; j < renderers.Length; j++)
+        {
+            renderers[j].positionCount = SimSteps;
+        }
+
+        // Start simulation
         for (int i = 0; i < SimSteps; i++)
         {
             physicsScene.GetPhysicsScene().Simulate(Time.fixedDeltaTime);
 
-            //Instantiate a trajectory_prefab for each step the sim_whiteball takes into the simulation
-            GameObject new_point = Instantiate(whiteball_trajectory_prefab);
-            new_point.transform.position = sim_ball.transform.position;
-            sim_steps.Add(new_point);
+            // Add positions to line renderer
+            realBallLineRenderer.SetPosition(i, sim_ball.transform.position);
+            for (int j = 0; j < renderers.Length; j++)
+            {
+                renderers[j].SetPosition(i, simOthers[j].transform.position);
+            }
         }
 
+        // Destroy balls
         Destroy(sim_ball);
+        for (int i = 0; i < simOthers.Count; i++)
+        {
+            Destroy(simOthers[i]);
+        }
     }
 }
 
