f08626974e668a4b1a0e4a63159bc8a67f93a2a2
Michele Rossi <mikymod@gmail.com>
2021-12-21 01:29:43 +0100

    add homework 40

--- Assets/Scripts/UI/ConsoleMng_Start.cs
+++ Assets/Scripts/UI/ConsoleMng_Start.cs
@@ -9,12 +9,23 @@ public class ConsoleMng_Start : MonoBehaviour
 {
     public TextMeshProUGUI TextConsole;
     public TMP_InputField InField;
+    public Image Background;
+    public ScrollRect ScrollView;
+    public int maxTextLen = 512;
     bool isFocused = false;
+
+    private void Start()
+    {
+        InField.Select();
+        InField.ActivateInputField();        
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return) && isFocused)
+        {
             SubmitCmd(InField.text);
+        }
         isFocused = InField.isFocused;
     }
 
@@ -37,9 +48,80 @@ public class ConsoleMng_Start : MonoBehaviour
 
     public void SubmitCmd(string newCmd)
     {
-        TextConsole.text += "\n<b>" + newCmd + "</b>";
+        // Avoid empty command
+        if (newCmd == "")
+        {
+            return;
+        }
+
+        // Autoclear
+        if (TextConsole.text.Length > maxTextLen)
+        {
+            TextConsole.text = "";
+        }
+
+        TextConsole.text += "\n<color=white><b>" + newCmd + "</b>";
         InField.text = "";
+
         //Set again the focus on the InputField
+        InField.Select();
+        InField.ActivateInputField();
+
         //...
+        newCmd = newCmd.ToLower();
+        var splittedCmd = newCmd.Split(' ');
+        switch(splittedCmd[0])
+        {
+            case "font":
+                if (splittedCmd.Length == 2)
+                {
+                    if (int.TryParse(splittedCmd[1], out int size))
+                    {
+                        TextConsole.text += "\n<color=green><b>OK</b>";
+                        TextConsole.fontSize = size;
+                    }
+                    else
+                    {
+                        TextConsole.text += "\n<color=red><b>Wrong parameter. Must be a number</b>";
+                    }
+                }
+                else
+                {
+                    TextConsole.text += "\n<color=red><b>Wrong format. Usage: font num</b>";
+                }
+                break;
+            case "background":
+                if (splittedCmd.Length == 4)
+                {
+                    int.TryParse(splittedCmd[1], out int red);
+                    int.TryParse(splittedCmd[2], out int green);
+                    int.TryParse(splittedCmd[3], out int blue);
+                    if ((red >= 0 && red < 256) && (green >= 0 && green < 256) && (blue >= 0 && blue < 256))
+                    {
+                        Background.color = new Color(Mathf.Clamp01(red), Mathf.Clamp01(green), Mathf.Clamp01(blue));
+                        TextConsole.text += "\n<color=green><b>OK</b>";
+                    }
+                    else
+                    {
+                        TextConsole.text += "\n<color=red><b>Wrong parameters: 0 <= x <= 255</b>";
+                    }
+                }
+                else
+                {
+                    TextConsole.text += "\n<color=red><b>Wrong format. Usage: background num num num</b>";
+                }
+                break;
+            case "clear":
+                TextConsole.text = "";
+                break;
+            default:
+                TextConsole.text += "\n<color=red><b>Unknown command</b>";
+                break;
+        }
+
+        // Scroll to bottom
+        Canvas.ForceUpdateCanvases();
+        ScrollView.verticalNormalizedPosition = 0f;
+        Canvas.ForceUpdateCanvases();
     }
 }
